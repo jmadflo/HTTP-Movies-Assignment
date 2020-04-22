ade3f407ed0be47d684f5302855dfc7fbf6b6968
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-22 10:27:56 -0700

    successfully pushed to update edit form

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,40 +1,43 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import MovieCard from "./MovieCard";
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import { useParams, useHistory } from "react-router-dom"
+import MovieCard from "./MovieCard"
 
-function Movie({ addToSavedList }) {
-  const [movie, setMovie] = useState(null);
-  const params = useParams();
+function Movie ({addToSavedList}) {
+  const [movie, setMovie] = useState(null)
+  const params = useParams()
+  const history = useHistory()
 
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then((res) => setMovie(res.data))
-      .catch((err) => console.log(err.response));
-  };
+      .catch((err) => console.log(err.response))
+  }
 
   const saveMovie = () => {
-    addToSavedList(movie);
-  };
+    addToSavedList(movie)
+  }
+
+  const goToEditMovie = () => {
+    history.push(`update-movie/${params.id}`)
+  }
 
   useEffect(() => {
-    fetchMovie(params.id);
-  }, [params.id]);
+    fetchMovie(params.id)
+  }, [params.id])
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>Loading movie information...</div>
   }
 
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
-
-      <div className="save-button" onClick={saveMovie}>
-        Save
-      </div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
+      <div className="edit-button" onClick={goToEditMovie}>Edit</div>
     </div>
-  );
+  )
 }
 
-export default Movie;
+export default Movie
